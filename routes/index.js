76c03e55ff5427abf53c15d057ebc5170ee2dfc7
cpkopp <cameronpkopp@gmail.com>
2022-11-26 18:55:55 -1000

    pizza front end

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const htmlRoutes = require('./html/html-routes');
 const apiRoutes = require('./api');
 
-router.use('./api', apiRoutes);
+router.use('/api', apiRoutes);
 router.use('/', htmlRoutes);
 
 router.use((req, res) => {
